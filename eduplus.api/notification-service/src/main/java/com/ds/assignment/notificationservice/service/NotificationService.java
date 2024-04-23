659334bcfde857f0b6055c57d396c1597b970a17
It21258794 <it21258794@my.sliit.lk>
2024-04-23 11:01:43 +0530

    Remove Twilio Account String Identifier

---
+++ eduplus.api/notification-service/src/main/java/com/ds/assignment/notificationservice/service/NotificationService.java
@@ -0,0 +1,10 @@
+package com.ds.assignment.notificationservice.service;
+
+import com.ds.assignment.notificationservice.dto.EmailRequest;
+import com.ds.assignment.notificationservice.dto.SmsRequest;
+import org.springframework.stereotype.Service;
+
+public interface NotificationService {
+   String sendSms(SmsRequest smsRequest);
+   void sendEmail(EmailRequest emailRequest);
+}
